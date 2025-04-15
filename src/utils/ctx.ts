0d5719ed4f9f963b55773d35286405604629535f
Tanju Erinmez <terinmez@adobe.com>
2025-04-16 00:18:02 +0200

    fixed endpoint path

--- src/utils/ctx.ts
+++ src/utils/ctx.ts
@@ -9,13 +9,14 @@ export interface ContentApiContext {
 }
 
 export function getContentApiContext(env: Bindings, programId: string, envId: string): ContentApiContext {
-  const endpointPagesPath = '/adobe/experimental/aspm-expires-20251231/pages';
   let pagesEndpointUrl = null;
   const headers: Headers = new Headers();
   if (env.WORKER_ENV === 'local') {
+    const endpointPagesPath = '/adobe/pages';
     pagesEndpointUrl = `http://localhost:8787${endpointPagesPath}`;
     headers['X-ADOBE-ROUTING'] = `program=${programId},environment=${envId}`;
   } else {
+    const endpointPagesPath = '/adobe/experimental/aspm-expires-20251231/pages';
     pagesEndpointUrl = `https://author-p${programId}-e${envId}.adobeaemcloud.com${endpointPagesPath}`;
   }
   console.log("Headers content:");
