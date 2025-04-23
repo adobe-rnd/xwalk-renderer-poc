97d32d1f3ef15627636afc31a6365b2942d6a25b
Tanju Erinmez <terinmez@adobe.com>
2025-04-23 14:17:25 +0200

    misc

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,9 @@ const openapi = fromHono(app, {
 
 // Path example: /xwalkpages/p130360_1272151_1534567d-9937-4e40-85ff-369a8ed45367/main/foobar/index.html
 //      programId------------^
-//      envId ----------------------^
-//      siteId --------------------------------^
-//      branch -----------------------------------------------------------------^
+//      envId ------------------------^
+//      siteId ------------------------------------^
+//      branch -------------------------------------------------------------------^
 //      path (foobar/index.html) --------------------------------------------------------^
 openapi.get("/xwalkpages/:xwalkPageId/:branch/", PageFetchById);
 openapi.get("/xwalkpages/:xwalkPageId/:branch/:path", PageFetchById);
